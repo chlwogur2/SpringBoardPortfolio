fb45864806dc674d981f124ab867ad6b0669c4d2
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-04-02 14:42:39 +0900

    마지막 발악

--- src/main/java/my_portfolio/portfolio/PortfolioApplication.java
+++ src/main/java/my_portfolio/portfolio/PortfolioApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableJpaRepositories("my_portfolio")
 public class PortfolioApplication {
 
 	public static void main(String[] args) {

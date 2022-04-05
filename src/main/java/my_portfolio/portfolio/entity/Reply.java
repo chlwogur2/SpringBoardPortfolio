9540eeffeb98a1dca76381d87098f097de6eb864
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-04-05 23:59:25 +0900

    reply 주석

--- src/main/java/my_portfolio/portfolio/entity/Reply.java
+++ src/main/java/my_portfolio/portfolio/entity/Reply.java
@@ -1,31 +1,31 @@
-package my_portfolio.portfolio.entity;
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class Reply extends BaseTimeEntity{
-
-    @Id @GeneratedValue
-    @Column(name = "reply_id")
-    private Long id;
-
-    private String content;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "comment_id")
-    private Comment comment;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    public Reply(String content) {
-        this.content = content;
-    }
-}
+//package my_portfolio.portfolio.entity;
+//
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Getter
+//public class Reply extends BaseTimeEntity{
+//
+//    @Id @GeneratedValue
+//    @Column(name = "reply_id")
+//    private Long id;
+//
+//    private String content;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "comment_id")
+//    private Comment comment;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//    public Reply(String content) {
+//        this.content = content;
+//    }
+//}
--- src/main/java/my_portfolio/portfolio/service/CommentService.java
+++ src/main/java/my_portfolio/portfolio/service/CommentService.java
@@ -18,4 +18,5 @@ public class CommentService {
         commentRepository.deleteById(commentId);
     }
 
+
 }
